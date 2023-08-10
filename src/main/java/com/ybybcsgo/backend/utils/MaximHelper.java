4a04ea1a3001dfeee492be0b48c9f2e68b4e0638
Kroytz <i@imomoe.cn>
2023-08-10 16:55:22 +0800

    Maxim auto parser complete

--- src/main/java/com/ybybcsgo/backend/utils/MaximHelper.java
+++ src/main/java/com/ybybcsgo/backend/utils/MaximHelper.java
@@ -7,10 +7,11 @@ import java.util.Random;
 
 public class MaximHelper {
 
+    // Now It's auto generate Maxim!
     public static void ConvertFullChatRecordToMaxim()
     {
         String path = "./chat.txt";
-        String outputPath = "./maximOut.txt";
+        String outputPath = "./maxims.txt";
 
         // Initialize file to write
         File outFile = new File(outputPath);
@@ -23,7 +24,68 @@ public class MaximHelper {
 
         if (outWriter != null)
         {
+            File file = new File(path);
+            InputStreamReader reader = null; // encoding
+            try {
+                reader = new InputStreamReader(new FileInputStream(file),"UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
 
+            //字符输入流中读取文本并缓冲字符
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line = null;
+
+            boolean fetching = false;
+            boolean isFetchingFirstLine = false;
+            try {
+                while ((line = bufferedReader.readLine()) != null) {
+
+                    if (line.contains("[图片]") || line.contains("[表情]"))
+                        continue;
+
+                    if (line.isBlank()) {
+
+                        // Stop fetching when meet space
+                        if (fetching) {
+                            fetching = false;
+
+                            // end record
+                            // if first line is blank, ignore this record.
+                            if (!isFetchingFirstLine) {
+                                outWriter.println("");
+                                outWriter.println("");
+                                outWriter.flush();
+                            }
+
+                            // reset
+                            isFetchingFirstLine = false;
+                        }
+
+                        continue;
+                    }
+
+                    if (fetching) {
+                        // use print to connect
+                        outWriter.print(line);
+                        isFetchingFirstLine = false;
+                    }
+                    else {
+                        // Found sb Wadu's record, start fetch whole record
+                        if (line.contains("(2070239158)")) {
+                            fetching = true;
+                            isFetchingFirstLine = true;
+                        }
+                    }
+                }
+
+                bufferedReader.close();
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
 
     }
